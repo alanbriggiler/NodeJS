8d21d7d359512b3c69f60a7c9c2e3fa8373d9b78
Alanbriggiler <Siglo21_>
2023-05-31 16:31:54 -0300

    Agregamos Db y middleware

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { logging } = require("./middleware");
+const { userRouter, authRouter } = require("./routes");
+const { initializeDB } = require("./config/dbConfig");
+const { authMiddleware } = require("./middleware/authentication-jwt");
+
+const PORT = 8090;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(logging);
+
+app.use("/user", authMiddleware, userRouter);
+
+app.use("/login", authRouter);
+
+app.listen(PORT, async () => {
+  await initializeDB();
+  console.log(
+    `Escuchando peticiones en el puerto: ${PORT}`
+  );
+});
