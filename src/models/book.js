c56ad6189b05ce9eeead6e8cf7e3032b5e74e66e
Alanbriggiler <Siglo21_>
2023-05-31 18:27:59 -0300

    Agregue book como models

---
+++ src/models/book.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/dbConfig");
+const Library = require("./library");
+
+const Book = sequelize.define("Books", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  isbn: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true,
+  },
+  titulo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  autor: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  year: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+Book.belongsTo(Library, { foreignKey: "libraryId" });
+Library.hasMany(Book, { foreignKey: "libraryId" });
+
+module.exports = Book;
